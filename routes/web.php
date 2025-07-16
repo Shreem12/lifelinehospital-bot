fb08756afe36812f0e2c35de42fd31184c9d150d
Shrikant Maid <shrikantmaid9@gmail.com>
2025-07-16 21:11:36 +0530

    Add /fix route to run artisan commands

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
+use Illuminate\Support\Facades\Artisan;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/fix', function () {
+    Artisan::call('key:generate');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('storage:link');
+    return 'Fix commands run!';
 });
